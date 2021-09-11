d76de86a6f7ecf28d2c9b5fef964d51f9b852444
Danielle <dany.sa1@hotmail.com>
2021-09-10 21:59:05 -0300

    feat: exercicios

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var nome = "Danielle Souza";
+(function(){
+
+/*var nome = "Danielle Souza";
 var idade = 36;
 alert("Bem vinda " + nome);
 
@@ -7,3 +9,22 @@ var data = new Date;
 
 alert (data);
 
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+alert(soma(2,3));
+
+function setReplace (frase, nome, novo_nome) {
+    return frase.replace (nome, novo_nome);
+}
+
+document.write(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+function validaIdade (idade, )
+
+
+
+})();
